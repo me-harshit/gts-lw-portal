698b0d8180a5081e75724983b1f4b93a6ff11cec
me-harshit <iamhtgrt@gmail.com>
2026-05-21 15:55:28 +0530

    Make API_URL dynamic to fix CORS/loopback errors

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,3 +1,9 @@
+const isProduction = window.location.hostname !== 'localhost';
+
+export const API_URL = isProduction 
+    ? 'https://lw.gts.ai' 
+    : 'http://localhost:5000';
+
 export const PROJECTS = {
   OFFICE: "64469240-0c5d-471e-9008-9ccbf03672a3",
   HOUSE: "730284cd-4b8b-4975-a8bd-28df1e7aaa06"
